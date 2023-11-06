1f088e2fe6df2c990d652b56f1d9d77081b2f9a2
Pulkit Methaniya <guptapulkit71@gmail.com>
2023-11-07 00:50:23 +0530

    new commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://pulkit:123@cluster0.xofh23l.mongodb.net/new?retryWrites=true';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
